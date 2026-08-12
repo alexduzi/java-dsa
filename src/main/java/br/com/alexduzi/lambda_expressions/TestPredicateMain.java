88b200e374aa7b3282c3a66d17836b5886a450fa
Alex Duzi <duzihd@gmail.com>
2026-08-12 15:55:50 -0300

    predicate

---
+++ src/main/java/br/com/alexduzi/lambda_expressions/TestPredicateMain.java
@@ -0,0 +1,30 @@
+package br.com.alexduzi.lambda_expressions;
+
+import java.util.function.BiPredicate;
+import java.util.function.Predicate;
+
+// custom function interface
+interface Evaluate<T> {
+    boolean isNegative(T i);
+}
+
+public class TestPredicateMain {
+    public static void main(String[] args) {
+        // using the custom function interface
+        Evaluate<Integer> lambda = i -> i > 0;
+        System.out.println("1 > 0 " + lambda.isNegative(1));
+        System.out.println("-1 > 0 " + lambda.isNegative(-1));
+
+        // When you want to filter/match
+        // Predicate<T> is a functional interface i.e. one abstract method
+        // boolean test(T t)
+        Predicate<Integer> predLambda = i -> i > 0;
+        System.out.println("1 > 0 " + predLambda.test(1));
+        System.out.println("-1 > 0 " + predLambda.test(-1));
+
+        // BiPredicate<T> is a functional interface i.e. one abstract method
+        // boolean test(T t)
+        BiPredicate<String, Integer> checkLength = (str, len) -> str.length() == len;
+        System.out.println("Vatican city has length of 8: " + checkLength.test("Vatican city", 8));
+    }
+}
