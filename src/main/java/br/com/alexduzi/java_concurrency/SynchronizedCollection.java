46e8e761216422d91b7c3f2b11d7a100ed8eafe9
Alex Duzi <duzihd@gmail.com>
2026-08-20 16:23:59 -0300

    SynchronizedCollection

--- src/main/java/br/com/alexduzi/java_concurrency/SynchronizedCollection.java
+++ src/main/java/br/com/alexduzi/java_concurrency/SynchronizedCollection.java
@@ -5,6 +5,15 @@ import java.util.Collections;
 import java.util.List;
 
 public class SynchronizedCollection {
+    // Usefull if you are given an existing non-concurrent collection
+    // and you want to share it among several threads
+
+    // However, if you know when you are creating your collection that you require
+    // concurrency your collection that you requre concurrency across threads, use
+    // the concurrenct collections outlined in the overview (better performance)
+
+    // Note: synchronized collections also throw ConcurrentModificationException if you try to modify
+    // them inside a loop (unlike concurrenct collections)
     public static void main(String[] args) {
         List<String> dogTypes = new ArrayList<>();
         dogTypes.add("German Shepherd");
